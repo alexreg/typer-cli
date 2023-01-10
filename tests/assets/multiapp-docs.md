55b39dedccba0e4fffb5e8dccabb15a6796e71bb
Alexander Regueiro <alexreg@gmail.com>
2023-01-10 00:14:07 +0000

    ⬆ Update dependency *typer-cloup* to 0.11.0

--- tests/assets/multiapp-docs.md
+++ tests/assets/multiapp-docs.md
@@ -10,8 +10,6 @@ $ multiapp [OPTIONS] COMMAND [ARGS]...
 
 **Options**:
 
-* `--install-completion`: Install completion for the current shell.
-* `--show-completion`: Show completion for the current shell, to copy it or customize the installation.
 * `--help`: Show this message and exit.
 
 The end
--- typer_cloup_cli/main.py
+++ typer_cloup_cli/main.py
@@ -140,7 +140,6 @@ def maybe_add_run_to_cli(cli: click.Group) -> None:
         if state.file or state.module:
             obj = get_typer_from_state()
             if obj:
-                obj._add_completion = False
                 click_obj = typer.main.get_command(obj)
                 click_obj.name = "run"
                 if not click_obj.help:
